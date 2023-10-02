6b8c73ab1e4a4159c6c185caae48038cedbc8bb7
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-10-02 16:37:47 +0500

    Task2

---
+++ Tyuiu.KomarovMI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMI.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
